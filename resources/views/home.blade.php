42c0b05ebcaa4c2900ed2b2f71c9e5a468a2c7af
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-06-14 19:47:04 +0600

    simple invoice design added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,12 +9,12 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <a href="{{route('invoices.create')}}" class="btn btn-primary">Add New Invoice</a>
                 </div>
             </div>
         </div>
