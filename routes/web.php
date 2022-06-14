401f214be664afeb539f72ed3f816ef6302cd696
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-06-14 19:23:58 +0600

    controller and route initialize

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\InvoicesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +21,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('invoices/create', [InvoicesController::class, 'create'])->name('invoices.create');
+});
