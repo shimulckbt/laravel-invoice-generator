9e77f5eb23596e9dd85ce834ac7634df73460103
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-06-24 23:22:08 +0600

    junk removed

--- app/Http/Controllers/InvoicesController.php
+++ app/Http/Controllers/InvoicesController.php
@@ -21,11 +21,6 @@ class InvoicesController extends Controller
 
     public function store(Request $request)
     {
-        // dd($request->customer);
-        // dd($request->invoice);  
-
-        // $customer = Customer::create($request->customer);
-
         $invoice = Invoice::create($request->invoice);
         for ($i = 0; $i < count($request->product); $i++) {
             if (isset($request->quantity[$i]) && isset($request->price[$i])) {
@@ -37,29 +32,17 @@ class InvoicesController extends Controller
                 ]);
             }
         }
-
-        // for ($i = 0; $i < count($request->customer_fields); $i++) {
-        //     if (isset($request->customer_fields[$i]['field_key']) && isset($request->customer_fields[$i]['field_value'])) {
-        //         CustomerField::create([
-        //             'customer_id' => $customer->id,
-        //             'field_key' => $request->customer_fields[$i]['field_key'],
-        //             'field_value' => $request->customer_fields[$i]['field_value'],
-        //         ]);
-        //     }
-        // }
         return route('home');
     }
 
     public function show(Invoice $invoice)
     {
-        // dd($invoice->id);
         return view('invoices.show', compact('invoice'));
     }
 
     public function download($invoice_id)
     {
         $invoice = Invoice::findOrFail($invoice_id);
-        // dd($invoice->all());
         $pdf = FacadePdf::loadView('invoices.pdf', compact('invoice'));
 
         return $pdf->download('invoice.pdf');
